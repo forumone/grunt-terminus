b155b3cfae3bb61753dc62d8d141ec8d77dfa6e9
William Hurley <whurley@forumone.com>
2017-08-09 13:21:38 -0400

    Adding index.js so it can be used with require()

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+ * grunt-terminus
+ * https://github.com/forumone/grunt-terminus
+ *
+ * Copyright (c) 2017 William Hurley
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = require('./tasks/terminus');
